bbef86951ca9c97a7a76871f96615c28d9c0aab2
Abderrahim <abdedarghal111@gmail.com>
2023-04-03 08:06:32 +0200

    this is autosave from 'autoguardar en git.bat'

--- autoguardar en git.bat
+++ autoguardar en git.bat
@@ -1,7 +1,11 @@
 @echo off
-echo "test"
-echo %~dp0
+title "Autoguardado en git del proyecto 'prueba'"
+echo "Starting save"
+echo "----------------------------------------------------------"
 git add %~dp0*
 git commit -m "this is autosave from 'autoguardar en git.bat'"
 git push
+echo "----------------------------------------------------------"
+echo "Save ended"
 pause
+exit
