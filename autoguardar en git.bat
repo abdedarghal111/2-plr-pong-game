03b6e465e0500234c9e5f6520175568760b268e3
Abderrahim <abdedarghal111@gmail.com>
2023-04-04 07:49:25 +0200

    completado(from 'autoguardar en git.bat')

--- autoguardar en git.bat
+++ autoguardar en git.bat
@@ -2,7 +2,7 @@
 title Autoguardado en git del proyecto "prueba"
 color 0B
 set /P message="Insert commit comment:"
-set autosave=(This is autosave from 'autoguardar en git.bat'.)
+set autosave=(from 'autoguardar en git.bat')
 set commit=%message%%autosave%
 echo Starting save
 echo ----------------------------------------------------------
