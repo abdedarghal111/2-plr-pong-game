4c74b5cf0e842b88598f53ef6bc53f0e5ab3bbbb
Abderrahim <abdedarghal111@gmail.com>
2023-04-04 07:48:19 +0200

    corrección del .bat(This is autosave from 'autoguardar en git.bat'.)

--- autoguardar en git.bat
+++ autoguardar en git.bat
@@ -1,11 +1,13 @@
 @echo off
 title Autoguardado en git del proyecto "prueba"
 color 0B
-set /P commit = "Insert commit comment:"
+set /P message="Insert commit comment:"
+set autosave=(This is autosave from 'autoguardar en git.bat'.)
+set commit=%message%%autosave%
 echo Starting save
 echo ----------------------------------------------------------
 git add %~dp0*
-git commit -m "%commit%(This is autosave from 'autoguardar en git.bat'.)"
+git commit -m "%commit%"
 git push
 echo ----------------------------------------------------------
 echo Save ended
